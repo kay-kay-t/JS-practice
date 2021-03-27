393c684751e3d81e64d6d7503ff83f46c16c9484
kay-kay-t <belkaa.ksu@gmail.com>
2021-03-27 16:12:19 -0500

    1

--- index2.js
+++ index2.js
@@ -1,36 +1,36 @@
-// for (let i = 1; i <= 5; i++) {
-//     if (i % 2 !== 0) console.log (i);
-// }
+for (let i = 1; i <= 5; i++) {
+    if (i % 2 !== 0) console.log (i);
+}
 
-// let i = 0;
-// while (i <= 5) {
-//     if (i % 2 !== 0) console.log (i);   
-//     i++;
-// }
+let i = 0;
+while (i <= 5) {
+    if (i % 2 !== 0) console.log (i);   
+    i++;
+}
 
-// let i = 0;
-// do {
-//     if (i % 2 !== 0) console.log (i);   
-//     i++; 
-// } 
-// while (i <= 5);
+let i = 0;
+do {
+    if (i % 2 !== 0) console.log (i);   
+    i++; 
+} 
+while (i <= 5);
 
-// // for..in loop for iterating over properties in an object
-// const person = {
-//     name: 'K',
-//     age:25
-// };
+// for..in loop for iterating over properties in an object
+const person = {
+    name: 'K',
+    age:25
+};
 
-// for (let key in person)
-//    console.log(key, person[key]);
+for (let key in person)
+   console.log(key, person[key]);
 
-// const colors = ['red', 'blue', 'green'];
-// for (let index in colors)
-//    console.log(index, colors[index]);   
+const colors = ['red', 'blue', 'green'];
+for (let index in colors)
+   console.log(index, colors[index]);   
 
-// //for..off to iterate over items in array
-// for (let color of colors)
-//    console.log(color);
+//for..off to iterate over items in array
+for (let color of colors)
+   console.log(color);
 
 
 
